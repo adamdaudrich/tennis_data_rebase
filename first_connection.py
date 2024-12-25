1bf01add64d454ed0931191e280718994d5aee5d
root <root@vultr.guest>
2024-12-25 18:29:17 +0000

    update

--- first_connection.py
+++ first_connection.py
@@ -10,17 +10,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    players = ['Ivan Lendl','Andre Agassi', 'Stefan Edberg']
+    players = ['Ivan Lendl','Andre Agassi', 'Roger Federer']
     return render_template('index.html', player=players)
-
-
     #this will pass in all names in players array through that 'player' variable in the html file
 
-    #render templates takes a teplate file (like an html) and processes it using Jinja templating engine. You can pass variables to the template. it helps separate the application logic from the presentation layer. You store it in 'templates' folder of your flask app. call render_template in your route function.
+    #render templates takes a template file (like an html) and processes it using Jinja templating engine. You can pass variables to the template. it helps separate the application logic from the presentation layer. You store it in 'templates' folder of your flask app. call render_template in your route function.
 
 @app.route('/player/<p>')
 def player(p):
-    return "I am " + str(p)
+    #Route for dynamic player pages
+    if p == "Roger Federer":
+        #Call a specific function from file roger_2003.py
+        from roger_2003 import get_federer_matches
+        
+    # Get Federer match data
+        federer_matches = get_federer_matches()
+
+    # Convert the DataFrame to a list of dictionaries for rendering
+        data = federer_matches.to_dict(orient='records')
+
+    # Render the HTML template with data
+        return render_template('roger_2003.html', matches=data)
+
+    else:
+        return "I am " + str(p)
 
 
 #if we want to run this locally, test for development purposes
